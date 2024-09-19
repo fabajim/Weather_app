7f8c21eee603a56949f962e4e8901750f9f9b35c
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2024-09-19 14:19:02 -0700

    Updated City and Weather class, City class no longer calls its own functions, evertyhing is controlled from WeatherApp class. Included data validation for api response.

--- src/WeatherApp.py
+++ src/WeatherApp.py
@@ -30,36 +30,38 @@ def getData(kelvin):
         return celsius
 
 
+class CityNotFound(Exception):
+    pass
+
+
 class City:
     def __init__(self, city) -> None:
         self.city = city
-        self.data = self._getResponse()
+        self.data = None
         # order of temps: current, minimum, maximum
         self.c_temps = []
         self.f_temps = []
-        self._convertAndSetTemps()
-
-    def _getResponse(self) -> None:
+        
+    def set_data(self) -> None:
         url = BASE_URL + "appid=" + API_KEY + "&q=" + self.city
-        return requests.get(url).json()
+        self.data = requests.get(url).json()
 
-    def getName(self) -> str:
+    def get_name(self) -> str:
         return self.city
 
-    def _convertAndSetTemps(self) -> tuple:
+    def convert_and_set_temps(self) -> None:
         cur = self.data['main']['temp']
         low = self.data['main']['temp_min']
         max = self.data['main']['temp_max']
         temps = [cur, low, max]
-        for temp in temps:
-            temp = getData(temp)
         self.set_c_temps(temps)
         self.set_f_temps()
 
     def set_c_temps(self, temps) -> None:
         """Insert celsius order: cur, min, max"""
         for val in temps:
-            self.c_temps.append(val)
+            c = getData(val)
+            self.c_temps.append(c)
 
     def set_f_temps(self) -> None:
         """Insert fahrenheit order: cur, min, max"""
@@ -68,16 +70,47 @@ class City:
 
     def get_f_temps(self) -> list:
         return self.f_temps
-    
+
     def get_c_temps(self) -> list:
         return self.c_temps
+    
+    def get_data(self):
+        return self.data
 
 
 class WeatherApp:
-    def __init__(self, city) -> None:
+    def __init__(self) -> None:
         self.cities = {}
-        self.addCity(city)
 
-    def addCity(self, name) -> None:
+    def add_city(self, name) -> None:
         self.cities[name] = City(name)
 
+    def set_city_data(self, city):
+        cur_city = self.cities[city]
+        cur_city.set_data()
+        data = cur_city.get_data()
+        if not self.validate_data(data):
+            return 0
+        cur_city.convert_and_set_temps()
+        return 1
+
+    def validate_data(self, data):
+        code = int(data['cod'])
+        if code >= 400:
+            return 0
+        return 1
+
+    def display_F_temps(self, city) -> None:
+        f_temps = self.cities[city].get_f_temps()
+        self.show_table(f_temps, city, 'F')
+
+    def display_C_temps(self, city) -> None:
+        c_temps = self.cities[city].get_c_temps()
+        self.show_table(c_temps, city, 'C')
+
+    def show_table(self, temps, city, type) -> None:
+        """Uses a table to show temps"""
+        table = PrettyTable(['City', 'Current', 'Lows', 'Highs'])
+        table.add_row([city, f'{int(temps[0])}{type}\xb0', f'{int(temps[1])}'
+                       f'{type}\xb0', f'{int(temps[2])}{type}\xb0'])
+        print('\n', table, '\n')
